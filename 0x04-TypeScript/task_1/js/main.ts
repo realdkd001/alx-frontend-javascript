dbad5a6b14b3949df35a944764f45085ac0074e3
Dzrekey001 <dkdzrekey001@gmail.com>
2025-09-09 20:28:45 +0000

    fix: yearofexperience as optional

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -2,7 +2,7 @@ export interface Teacher {
     readonly firstName: string;
     readonly lastName: string;
     fullTimeEmployee: boolean;
-    yearOfExperience?: number;
+    yearsOfExperience?: number
     location: string;
     [key: string]: any;
 }
