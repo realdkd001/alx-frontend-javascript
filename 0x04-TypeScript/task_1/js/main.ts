93bf21b4da9cbd1f52144370818480064fa4cbd9
Dzrekey001 <dkdzrekey001@gmail.com>
2025-09-09 21:13:44 +0000

    feat: writing a class interface

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -18,3 +18,29 @@ export interface printTeacherFunction {
 function printTeacher({ firstName, lastName }: Teacher): string {
   return `${firstName}. ${lastName}`;
 }
+
+export interface StudentClassInterface {
+    firstName: string;
+    lastName: string;
+    workOnHomework(): string;
+    displayName(): string;
+}
+
+
+class StudentClass implements StudentClassInterface {
+    firstName: string;
+    lastName: string;
+    
+    constructor(firstName: string, lastName: string){
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    workOnHomework():string {
+        return "Currently working";
+    }
+
+    displayName():string {
+        return this.firstName;
+    }
+}
