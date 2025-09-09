fdf4d495efdee0ffe0043d9fa05839c0a2b4c966
Dzrekey001 <dkdzrekey001@gmail.com>
2025-09-09 20:47:14 +0000

    fix: printTeacher function

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -15,6 +15,6 @@ export interface printTeacherFunction {
     (firstName: string, lastName: string): string;
 }
 
-export function printTeacher(firstName: string, lastName: string): string {
-    return `${firstName.charAt(0)}. ${lastName}`;
+export function printTeacher(firstName, lastName)  {
+    return `${firstName}. ${lastName}`;
 }   
