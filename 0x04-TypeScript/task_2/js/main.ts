a8d8e8bfbb146e5a3f058028cda0ca1027a93bf4
Dzrekey001 <dkdzrekey001@gmail.com>
2025-09-09 21:58:02 +0000

    fix: createEmployee function logic

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -4,7 +4,7 @@ export interface DirectorInterface{
   workDirectorTasks(): string;
 }
 
-export interface TeacherInterface{
+interface TeacherInterface {
   workFromHome(): string;
   getCoffeeBreak(): string;
   workTeacherTasks(): string;
@@ -35,11 +35,13 @@ export class Teacher implements TeacherInterface {
 }
 
 export function createEmployee(salary: number | string): Director | Teacher {
-  if (typeof salary === "number" && salary < 500){
-    return new Teacher()
+  if (typeof salary === "number"){
+    if (salary < 500){
+      return new Teacher()
+    }
   }
   return new Director()
-  
+
   }
 
   console.log(createEmployee(200));
