92ee55eeedfc42de0ba96526f8836dbc4750e8b7
Dzrekey001 <dkdzrekey001@gmail.com>
2025-09-10 11:28:10 +0000

    Ambient Namespaces

---
+++ 0x04-TypeScript/task_3/js/crud.d.ts
@@ -0,0 +1,8 @@
+import { insertRow, deleteRow, updateRow  } from "./crud";
+import { RowID, RowElement} from "./interface";
+
+
+
+export function insertRow(row: RowElement): number;
+export function deleteRow(rowId: RowID): void;
+export function updateRow(rowId:RowID, row: RowElement): RowID;
