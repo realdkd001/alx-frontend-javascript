06052e2444d5307240146905cdcecabf4283ca3b
Dzrekey001 <dkdzrekey001@gmail.com>
2025-09-10 11:35:19 +0000

    fix: Types

--- 0x04-TypeScript/task_3/js/main.ts
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -7,10 +7,7 @@ const row: RowElement = {
   firstName: "Guillaume",
   lastName: "Salva",
 }
-const updatedRow: RowElement = {
-  ...row,
-  age: 23
-}
+const updatedRow: RowElement = {...row, age: 23}
 const newRowID: RowID = CRUD.insertRow(row);
 CRUD.updateRow(newRowID, updatedRow);
 CRUD.deleteRow(newRowID);
